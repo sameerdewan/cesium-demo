377a182471ca29d25010270baf5993467948409f
Sameer Dewan <59847976+sameerdewan@users.noreply.github.com>
2022-10-19 09:37:27 -0400

    Update hooks.js

--- src/hooks.js
+++ src/hooks.js
@@ -1,7 +1,7 @@
 import * as Cesium from "cesium";
 import { useEffect, useState } from "react";
 
-// This is a normal a$$ developer account. Feel free to steel this but tbh why not just use your own?
+// This is a normal a$$ developer account. Feel free to steal this but tbh why not just use your own?
 const accessToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2NWEyMGQ1NC1iMGQ2LTQ0ZGQtYjQwZS01YWU1ZTQ5M2RkODciLCJpZCI6MTExNjE4LCJpYXQiOjE2NjYxNDg2NjB9.tgwuiDEQLh7cltSppC3DrkycIhOaYM6Yf8cRI6rCBjk";
 
