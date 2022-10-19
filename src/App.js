4a05dbf370b7e3de0707bd862881c144701fa46f
Sameer Dewan <sameerdewan1994@gmail.com>
2022-10-19 08:59:19 -0400

    useCesium hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,52 @@
+import { useEffect } from "react";
+import { useCesium } from "./hooks";
+import * as Cesium from "cesium";
+
 function App() {
+  const viewer1 = useCesium("map-container-1");
+  const viewer2 = useCesium("map-container-2");
+
+  useEffect(() => {
+    if (viewer1) {
+      viewer1.scene.primitives.add(Cesium.createOsmBuildings());
+      viewer1.clock.currentTime = Cesium.JulianDate.fromIso8601(
+        "2020-09-01T16:23:19.06128571429871954Z"
+      );
+      viewer1.scene.globe.depthTestAgainstTerrain = true;
+
+      viewer1.camera.flyTo({
+        destination: Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+        orientation: {
+          heading: Cesium.Math.toRadians(0.0),
+          pitch: Cesium.Math.toRadians(-15.0),
+        },
+      });
+    }
+  }, [viewer1]);
+
+  useEffect(() => {
+    if (viewer2) {
+      viewer2.scene.primitives.add(Cesium.createOsmBuildings());
+      viewer2.clock.currentTime = Cesium.JulianDate.fromIso8601(
+        "2020-09-01T16:23:19.06128571429871954Z"
+      );
+      viewer2.scene.globe.depthTestAgainstTerrain = true;
+
+      viewer2.camera.flyTo({
+        destination: Cesium.Cartesian3.fromDegrees(-122.4140, 37.655, 400),
+        orientation: {
+          heading: Cesium.Math.toRadians(0.0),
+          pitch: Cesium.Math.toRadians(-15.0),
+        },
+      });
+    }
+  }, [viewer2]);
+
   return (
-    <div id="App" />
+    <div id="App">
+      <div id="map-container-1" />
+      <div id="map-container-2" />
+    </div>
   );
 }
 
---
+++ src/hooks.js
@@ -0,0 +1,29 @@
+import * as Cesium from "cesium";
+import { useEffect, useState } from "react";
+
+const accessToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2NWEyMGQ1NC1iMGQ2LTQ0ZGQtYjQwZS01YWU1ZTQ5M2RkODciLCJpZCI6MTExNjE4LCJpYXQiOjE2NjYxNDg2NjB9.tgwuiDEQLh7cltSppC3DrkycIhOaYM6Yf8cRI6rCBjk";
+
+Cesium.Ion.defaultAccessToken = accessToken;
+
+export function useCesium(targetID) {
+  const [viewer, setViewer] = useState();
+
+  useEffect(() => {
+    if (document.getElementById(targetID).childElementCount >= 1) return; 
+    setViewer(
+      new Cesium.Viewer(targetID, {
+        requestRenderMode: true,
+        maximumRenderTimeChange: Infinity,
+        timeline: false,
+        animation: false,
+        baseLayerPicker: false,
+        sceneModePicker: false,
+        terrainProvider: Cesium.createWorldTerrain(),
+        imageryProvider: Cesium.createWorldImagery(),
+      })
+    );
+  }, []); // eslint-disable-line
+
+  return viewer;
+}
